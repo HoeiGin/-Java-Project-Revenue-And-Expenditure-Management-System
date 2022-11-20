3698c781bfd6ad3191f521e71647b0d953d089dd
HoeiGin <108538401+HoeiGin@users.noreply.github.com>
2022-11-20 14:27:40 +0800

    Add files via upload

---
+++ Data/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` VALUES (1, 'find001', 'admin', 'admin123456', '13456788888');
+INSERT INTO `users` VALUES (2, '1ZAA2', 'Jerry', 'jerry', '12536589745');
+INSERT INTO `users` VALUES (3, '1ZZA2', 'ginheoi', 'ginheoi', '12345677888');
+INSERT INTO `users` VALUES (4, '3ZSZS', 'yanshape', 'yanshape', '12345688588');
+INSERT INTO `users` VALUES (5, '123ZS', 'testAccount', '123456', '12345699888');
+INSERT INTO `users` VALUES (6, '12ZZZ', 'test', 'test', '12345678888');
